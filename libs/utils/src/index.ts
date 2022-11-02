67cd6eef9b5f070ef2412f6cd0839392054b8958
SobkoAlex <alexfreetibet@gmail.com>
2022-11-02 13:03:07 +0700

    build: create monorepo with configs

---
+++ libs/utils/src/index.ts
@@ -0,0 +1,5 @@
+export * from './app.exceptions.filter';
+export * from './app.validation.pipe';
+export * from './app.response.interceptor';
+export * from './exceptions/app.exceptions';
+export * from './exceptions/exceptions.constants';
